2b13a12304d6381daeecc5414503227babe75be7
linuxmint-markdown <linuxmint546@gmail.com>
2020-12-12 11:34:58 +0530

    Rahul Dev Website is created

---
+++ django-simple-portfolio-master/core/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+    # override get context date method
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)  # first, call super get context data
+        context['about'] = About.objects.first()
+        context['services'] = Service.objects.all()
+        context['works'] = RecentWork.objects.all()
+        return context
+
+
